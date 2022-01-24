df590b3db9e5ba3595a9db0f1d3e4d7d4a689173
Vitaly Markov <vymarkov@gmail.com>
2022-01-24 11:25:19 +0000

    save work

--- libs/aws-cloud9-terraform/main.tf
+++ libs/aws-cloud9-terraform/main.tf
@@ -5,14 +5,29 @@ terraform {
       version = "~> 3.0"
     }
   }
-
-  backend "http" {
-  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
   region = "eu-central-1"
+}
+
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+module "terraform_state_backend" {
+  source = "cloudposse/tfstate-backend/aws"
+  # Cloud Posse recommends pinning every module to a specific version
+  version    = "0.38.1"
+  namespace  = "lo"
+  stage      = "development"
+  name       = "terraform"
+  attributes = ["state"]
+
+  terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
 }
 
 # data "aws_iam_user" "gitpod_user" {
