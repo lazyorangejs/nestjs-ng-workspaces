54959586baef9600aa95348ca49965e59ec2fc62
Vitaly Markov <vymarkov@gmail.com>
2022-01-24 10:49:01 +0000

    save work [skip ci]

--- libs/aws-cloud9-terraform/cloud9-ec2-instance.tf
+++ libs/aws-cloud9-terraform/cloud9-ec2-instance.tf
@@ -1,28 +1,98 @@
-data "aws_iam_instance_profile" "this" {
-  name = "foobar"
+module "labels" {
+  source = "cloudposse/label/null"
+  # Cloud Posse recommends pinning every module to a specific version
+  version = "0.25.0"
+
+  namespace   = "lazyorange"
+  environment = "development"
+  name        = "vscode-remote-workstation"
+  delimiter   = "-"
+  attributes  = []
+
+  tags = {
+    Terraform = true
+  }
 }
 
-module "cloud9_workstation" {
-  source  = "cloudposse/ec2-instance/aws"
-  version = "0.40.0"
+// https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/modules/iam-assumable-role
+// https://github.com/terraform-aws-modules/terraform-aws-iam/tree/master/examples/iam-assumable-role
+module "iam_assumable_role_workstation" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
+  version = "~> 4"
 
-  assign_eip_address          = true
+  trusted_role_services = [
+    "ec2.amazonaws.com"
+  ]
+
+  # trusted_role_arns = [
+  #   "arn:aws:iam::912888207281:user/gitpod"
+  # ]
+
+  # arn:aws:iam::912888207281:user/gitpod
+
+  create_role             = true
+  create_instance_profile = true
+
+  role_name         = "custom"
+  role_requires_mfa = false
+
+  custom_role_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonSSMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  ]
+  number_of_custom_role_policy_arns = 2
+}
+
+// https://github.com/terraform-aws-modules/terraform-aws-ec2-instance
+module "workstation-2" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+
+  name        = module.labels.id
+  cpu_credits = "unlimited"
+
+  ami                    = "ami-0994cdfed5f3888d6"
+  instance_type          = "t2.micro"
+  key_name               = "development"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-2faab34d"]
+  subnet_id              = "subnet-fd6b7780"
+
   associate_public_ip_address = true
-
-  ssh_key_pair  = "development"
-  instance_type = "t2.micro"
-  vpc_id        = "vpc-aa7a84c0"
-  subnet        = "subnet-fd6b7780"
-  # security_groups             = var.security_groups
-  instance_profile = data.aws_iam_instance_profile.this.name
-
-  name      = "vymarkov"
-  namespace = "eg"
-  stage     = "dev"
+  iam_instance_profile        = module.iam_assumable_role_workstation.iam_instance_profile_name
 
   # https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-agent-status-and-restart.html
   user_data = <<EOF
 #!/bin/bash
-sudo snap install amazon-ssm-agent --classic"
+sudo snap install amazon-ssm-agent --classic
+sudo snap install aws-cli --classic
 EOF
+
+  tags = module.labels.tags
 }
+
+# module "workstation" {
+#   source  = "cloudposse/ec2-instance/aws"
+#   version = "0.40.0"
+
+#   assign_eip_address          = true
+#   associate_public_ip_address = true
+
+#   ssh_key_pair  = "development"
+#   instance_type = "t2.micro"
+#   vpc_id        = "vpc-aa7a84c0"
+#   subnet        = "subnet-fd6b7780"
+
+#   # security_groups             = var.security_groups
+#   instance_profile = module.iam_assumable_role_workstation.iam_instance_profile_name
+
+#   name      = "vymarkov"
+#   namespace = "eg"
+#   stage     = "dev"
+
+#   # https://docs.aws.amazon.com/systems-manager/latest/userguide/ssm-agent-status-and-restart.html
+#   user_data = <<EOF
+# #!/bin/bash
+# sudo snap install amazon-ssm-agent --classic"
+# EOF
+# }
--- libs/aws-cloud9-terraform/main.tf
+++ libs/aws-cloud9-terraform/main.tf
@@ -5,6 +5,9 @@ terraform {
       version = "~> 3.0"
     }
   }
+
+  backend "http" {
+  }
 }
 
 # Configure the AWS Provider
@@ -12,60 +15,60 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-data "aws_iam_user" "gitpod_user" {
-  user_name = "cloud9-user"
-}
+# data "aws_iam_user" "gitpod_user" {
+#   user_name = "cloud9-user"
+# }
 
-// https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloud9_environment_ec2
-resource "aws_cloud9_environment_ec2" "demo" {
-  instance_type = "t2.micro"
-  name          = "demo"
+# // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/cloud9_environment_ec2
+# resource "aws_cloud9_environment_ec2" "demo" {
+#   instance_type = "t2.micro"
+#   name          = "demo"
 
-  automatic_stop_time_minutes = 30
+#   automatic_stop_time_minutes = 30
 
-  tags = {
-    user_name            = data.aws_iam_user.gitpod_user.user_name,
-    owner_emails_address = "vymarkov@gmail.com"
-  }
-}
+#   tags = {
+#     user_name            = data.aws_iam_user.gitpod_user.user_name,
+#     owner_emails_address = "vymarkov@gmail.com"
+#   }
+# }
 
-resource "aws_cloud9_environment_membership" "demo" {
-  environment_id = aws_cloud9_environment_ec2.demo.id
-  permissions    = "read-write"
-  user_arn       = data.aws_iam_user.gitpod_user.arn
-}
+# resource "aws_cloud9_environment_membership" "demo" {
+#   environment_id = aws_cloud9_environment_ec2.demo.id
+#   permissions    = "read-write"
+#   user_arn       = data.aws_iam_user.gitpod_user.arn
+# }
 
-data "aws_instance" "cloud9-instance" {
-  filter {
-    name   = "tag:aws:cloud9:environment"
-    values = [aws_cloud9_environment_ec2.demo.id]
-  }
-}
+# data "aws_instance" "cloud9-instance" {
+#   filter {
+#     name   = "tag:aws:cloud9:environment"
+#     values = [aws_cloud9_environment_ec2.demo.id]
+#   }
+# }
 
-data "aws_security_group" "cloud9_default_sg" {
-  name = element(data.aws_instance.cloud9-instance.security_groups[*], 0)
-}
+# data "aws_security_group" "cloud9_default_sg" {
+#   name = element(data.aws_instance.cloud9-instance.security_groups[*], 0)
+# }
 
-resource "aws_security_group_rule" "allow_ssh_from_developer_workstation" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["195.69.222.254/32"]
-  security_group_id = data.aws_security_group.cloud9_default_sg.id
-}
+# resource "aws_security_group_rule" "allow_ssh_from_developer_workstation" {
+#   type              = "ingress"
+#   from_port         = 22
+#   to_port           = 22
+#   protocol          = "tcp"
+#   cidr_blocks       = ["195.69.222.254/32"]
+#   security_group_id = data.aws_security_group.cloud9_default_sg.id
+# }
 
-output "owner_id" {
-  value = data.aws_iam_user.gitpod_user.user_id
-}
+# output "owner_id" {
+#   value = data.aws_iam_user.gitpod_user.user_id
+# }
 
-output "ssh_command" {
-  # value = "ssh ec2-user@${data.aws_instance["cloud9-instance"].public_ip}"
-  value = "ssh ec2-user@${data.aws_instance.cloud9-instance.public_ip}"
-}
+# output "ssh_command" {
+#   # value = "ssh ec2-user@${data.aws_instance["cloud9-instance"].public_ip}"
+#   value = "ssh ec2-user@${data.aws_instance.cloud9-instance.public_ip}"
+# }
 
-output "security_groups" {
-  value = element(data.aws_instance.cloud9-instance.security_groups[*], 0)
-}
+# output "security_groups" {
+#   value = element(data.aws_instance.cloud9-instance.security_groups[*], 0)
+# }
 
-// security_groups
+# // security_groups
